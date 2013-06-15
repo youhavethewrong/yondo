e460a4a91cf6544bbd34f9025f05723ec072694a
Negated <youhavethewrong@gmail.com>
2013-06-15 18:31:37 -0500

    cleaning up test

--- src/test/java/info/youhavethewrong/yondo/YondoResourceTest.java
+++ src/test/java/info/youhavethewrong/yondo/YondoResourceTest.java
@@ -1,15 +1,14 @@
 package info.youhavethewrong.yondo;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 
 import com.yammer.dropwizard.testing.ResourceTest;
 
 public class YondoResourceTest extends ResourceTest {
-	private final Quote testQuote = new Quote(0,
-			"Lichen, mistletoe, aloft<br>In the dying croft,<br>Leaf the winter trees.");
+	// private final Quote testQuote = new Quote(0,
+	// "Lichen, mistletoe, aloft<br>In the dying croft,<br>Leaf the winter trees.");
 	private final QuoteDAO dao = mock(QuoteDAO.class);
 
 	@Override
@@ -20,8 +19,8 @@ public class YondoResourceTest extends ResourceTest {
 	@Test
 	public void shouldRetrieveQuote() {
 		// TODO make sensible tests
-		//when(dao.findContentById())).thenReturn(testQuote.getContent());
-		//assertEquals("", client().resource("/quote").get(Quote.class));
+		// when(dao.findContentById())).thenReturn(testQuote.getContent());
+		// assertEquals("", client().resource("/quote").get(Quote.class));
 	}
 
 }
